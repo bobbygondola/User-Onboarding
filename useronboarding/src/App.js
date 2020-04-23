27c55fa264db8c406edc8e168edb9f4a07c1ff4e
bobbygondola <60513315+bobbygondola@users.noreply.github.com>
2020-04-22 21:57:17 -0400

    MVP

---
+++ useronboarding/src/App.js
@@ -0,0 +1,152 @@
+import React, { useState } from "react";
+import Form from "./Form";
+import "./App.css";
+
+//Todays Inputs
+import * as yup from "yup";
+import axios from "axios";
+
+const url = "https://reqres.in/api/users";
+
+//default Onboarding list
+const memberList = [
+  {
+    name: "Bobby",
+    email: "Bobby@LambdaSchool.com",
+    password: "chickenpox",
+    termsOfService: "Agree",
+  },
+];
+
+//Setting the inital empty form values
+const initialFormValues = {
+  //text inputs
+  name: "",
+  email: "",
+  password: "",
+  //checkbox
+  termsOfService: "",
+};
+
+//the shape of the validation errors object
+const initialFormErrors = {
+  name: "",
+  email: "",
+  password: "",
+  termsOfService: "",
+};
+
+// creating schema for validation
+const formSchema = yup.object().shape({
+  name: yup
+    .string()
+    .min(3, "username must have atleast 3 characters you goon!")
+    .required("username is required bro!"),
+  email: yup
+    .string()
+    .email("a valid email is required brosepf!")
+    .required("must input an email"),
+  password: yup
+    .string()
+    .min(5, "must input 5 characters at least!")
+    .required("password is required"),
+});
+
+////////////////////////////////////////////////////////////////////////
+
+function App() {
+  //member list set to state
+  const [members, setMembers] = useState(memberList);
+  //form values set to state
+  const [formValues, setFormValues] = useState(initialFormValues);
+  //errors section
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+
+  //input change
+  const onInputChange = (evt) => {
+    const name = evt.target.name;
+    const value = evt.target.value;
+
+    // set a new state for the whole form
+    setFormValues({ ...formValues, [name]: value });
+
+    yup
+      .reach(formSchema, name)
+      .validate(value)
+      .then(valid => {
+        // yoohoo, validates :)
+        // CLEAR ERROR
+        setFormErrors({
+          ...formErrors,
+          [name]: '',
+        })
+      })
+      .catch(err => {
+        // dangit, does not validate :(
+        // SET THE ERROR IN THE RIGHT PLACE
+        setFormErrors({
+          ...formErrors,
+          [name]: err.errors[0]
+        })
+      })
+  };
+
+  // onSubmit function
+  const onSubmit = (evt) => {
+    // don't allow the browser to reload!
+
+    evt.preventDefault();
+
+    const newMember = {
+      name: formValues.name,
+      email: formValues.email,
+      password: formValues.password,
+    };
+
+    newData(newMember);
+    setFormValues(initialFormValues);
+  };
+
+  const newData = (member) => {
+    axios
+      .post(url, member)
+      .then((res) => {
+        console.log(res.data);
+        // setFormValues(res.data)
+        setMembers([...members, res.data]);
+      })
+      .catch((err) => {
+        debugger;
+      });
+  };
+
+  console.log(members);
+  return (
+    <div className="App">
+      <h1>Onboarding Form</h1>
+
+      {members.map((member) => {
+        console.log(members)
+      
+        return (
+        <div key={member.name} id="card">
+          <p>Name: {member.name}</p>
+          <p>Email: {member.email}</p>
+        <p>Password: {member.password}</p>
+        <p>Terms of Service: {member.termsOfService}</p>
+        </div>)
+      })}
+
+      <Form
+        //props that we need to come back to
+        values={formValues}
+        onInputChange={onInputChange}
+        // onCheckboxChange={onCheckboxChange}
+        onSubmit={onSubmit}
+        errors={formErrors}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ useronboarding/src/Form.js
@@ -0,0 +1,77 @@
+import React from 'react';
+
+function Form(props) {
+    const {
+        values,
+        onInputChange,
+        onCheckboxChange,
+        onSubmit,
+        disabled,
+        errors,
+      } = props
+
+    return (
+      <div className="FormContainer">
+        <form onSubmit={onSubmit} >
+
+        <div className='errors'>
+        {errors.name}
+        {errors.email}
+        {errors.password}
+
+      </div>
+            
+        {/* USERNAME FIELD */}
+            <label>Username:
+        <input
+          value={values.name}
+          onChange={onInputChange}
+          
+          name='name'
+          type='text'
+        /></label>
+
+
+        <br></br>
+
+
+        {/* //EMAIL FIELD */}
+        <label>Email:
+        <input
+          value={values.email}
+          onChange={onInputChange}
+          name='email'
+          type='text'
+        /></label>
+
+
+        <br></br>
+
+
+        {/* PASSWORD FIELD */}
+        <label>Password:
+      <input
+          value={values.password}
+          onChange={onInputChange}
+          name='password'
+          type='text'
+        /></label>
+
+
+        <br></br>
+
+
+        {/* TERMS OF SERVICE FIELD */}
+        <label>Terms of Service
+           <label> <input type="radio" value="Agree"/> Agree</label>
+        </label>
+
+        <br></br>
+
+        <button onClick={onSubmit} >Submit</button>
+        </form>
+      </div>
+    );
+  }
+  
+  export default Form;
